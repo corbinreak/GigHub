64e2c128e0c4a190fed675f01af435eccad0d99e
Corbin Reak <corbinreak@gmail.com>
2026-02-06 15:00:28 -0700

    Finished signup form

--- components/SignUp.tsx
+++ components/SignUp.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, TextInput, Pressable, StyleSheet } from 'react-native';
+import { View, Text, TextInput, Pressable, StyleSheet, KeyboardAvoidingView, Platform, Touchable, Keyboard, TouchableWithoutFeedback } from 'react-native';
 import { Colors } from '../constants/Color';
 import * as Haptics from 'expo-haptics';
 
@@ -7,46 +7,58 @@ import * as Haptics from 'expo-haptics';
 function SignUp({onGoBack, setHasLoggedIn}: {onGoBack: () => void, setHasLoggedIn: React.Dispatch<React.SetStateAction<boolean>>} ) {
 
     return (
-        <View style={styles.SignUpWrapper}>
-            <View style={styles.SignUpContainer}>
-                <Text style={{fontSize: 24, marginBottom: 20, color: Colors.light.text}}>Create Your Account</Text>
-                <Text style={{fontSize: 16, marginBottom: 40, color: Colors.light.secondaryText}}>Sign Up To Start Tracking Miles!</Text>
-                <TextInput
-                   placeholder="Name"
-                   placeholderTextColor={Colors.light.secondaryText}
-                   style={styles.inputText} 
-                ></TextInput>
-                <TextInput
-                   placeholder="Email"
-                   placeholderTextColor={Colors.light.secondaryText}
-                   style={styles.inputText}
-                 ></TextInput>
-                 <TextInput
-                   placeholder="Password"
-                   placeholderTextColor={Colors.light.secondaryText}
-                   secureTextEntry={true}
-                   style={styles.inputText}
-                 ></TextInput>
-                 <TextInput
-                     placeholder="Confirm Password"
-                     placeholderTextColor={Colors.light.secondaryText}
-                     secureTextEntry={true}
-                     style={styles.inputText}
-                 ></TextInput>
-            </View>
+        <KeyboardAvoidingView style={styles.SignUpWrapper} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+          <TouchableWithoutFeedback onPress={Keyboard.dismiss}>  
+            <View style={styles.SignUpWrapper}>
+                <View style={styles.SignUpContainer}>
+                    <Text style={{fontSize: 24, marginBottom: 20, color: Colors.light.text}}>Create Your Account</Text>
+                    <Text style={{fontSize: 16, marginBottom: 40, color: Colors.light.secondaryText}}>Sign Up To Start Tracking Miles!</Text>
+                    <TextInput
+                    placeholder="Name"
+                    placeholderTextColor={Colors.light.secondaryText}
+                    style={styles.inputText} 
+                    ></TextInput>
+                    <TextInput
+                    placeholder="Email"
+                    placeholderTextColor={Colors.light.secondaryText}
+                    style={styles.inputText}
+                    ></TextInput>
+                    <TextInput
+                    placeholder="Password"
+                    placeholderTextColor={Colors.light.secondaryText}
+                    secureTextEntry={true}
+                    style={styles.inputText}
+                    ></TextInput>
+                    <TextInput
+                        placeholder="Confirm Password"
+                        placeholderTextColor={Colors.light.secondaryText}
+                        secureTextEntry={true}
+                        style={styles.inputText}
+                    ></TextInput>
+                </View>
+                
+                <View style={styles.footer}>
+                    <Pressable style={({pressed}) => [
+                        {backgroundColor: pressed ? '#0d1117' : '#273140',
+                        },
+                        {padding: 15, borderRadius: 10, width: '80%', alignItems: 'center', marginBottom: 20}
+                    ]}
+                    onPress={() => {Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium); setHasLoggedIn(true); onGoBack()}} 
+                    >
+                        <Text style={{color: '#fff', fontSize: 16, fontWeight: 'bold'}}>Sign Up</Text>
+                    </Pressable>
 
-            <View style={styles.footer}>
-                <Pressable style={({pressed}) => [
-                    {backgroundColor: pressed ? '#0d1117' : '#273140',
-                    },
-                    {padding: 15, borderRadius: 10, width: '80%', alignItems: 'center', marginBottom: 20}
-                 ]}
-                 onPress={() => {Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium); setHasLoggedIn(true); onGoBack()}} 
-                >
-                    <Text style={{color: '#fff', fontSize: 16, fontWeight: 'bold'}}>Sign Up</Text>
-                 </Pressable>
+                    <View style={{flexDirection: 'column', gap: 15, alignItems: 'center'}}>
+                        <Text style={{color: Colors.light.secondaryText}}>Already have an account?</Text>
+                        <Pressable onPress={() => {Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light); onGoBack()}}>
+                            <Text style={{color: '#10b981', fontWeight: 'bold'}}>Log In</Text>
+                        </Pressable>
+                    </View>
+                </View>
             </View>
-        </View>
+          </TouchableWithoutFeedback>
+        </KeyboardAvoidingView>
+        
     );
 }
 
